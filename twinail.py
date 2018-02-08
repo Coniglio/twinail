f26efb05884b4a1c51846d4b3502ea9d8f4427a6
Coniglio <halfway67@icloud.com>
2018-02-08 16:00:55 +0900

    Update twinail.py

--- twinail.py
+++ twinail.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+import configparser
 import urllib
 import oauth2 as oauth
 
@@ -11,6 +12,10 @@ consumer_key = ''
 consumer_secret = ''
 
 def oauth_request(url, key, secret, http_method='GET', post=body='', http_headers=None):
+    
+    config = configparser.ConfigParser()
+    config.read('twitter.ini')
+    
     consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     token = oauth2.Token(key=key, secret=secret)
     client = oauth2.Client(consumer, token)
@@ -18,6 +23,5 @@ def oauth_request(url, key, secret, http_method='GET', post=body='', http_header
     return content
 
 if __name__ == '__main__':
-
     home_timeline = oauth_req( 'https://api.twitter.com/1.1/statuses/home_timeline.json', 'abcdefg', 'hijklmnop' )
     print(home_timeline)
