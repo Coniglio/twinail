156bf69313d0d6a44e4c4c6a306046d57902d024
Coniglio <halfway67@icloud.com>
2018-02-08 15:48:27 +0900

    Create twinail.py

---
+++ twinail.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+import urllib
+import oauth2 as oauth
+
+request_token_url = ''
+access_token_url = ''
+authenticate_url = ''
+callback_url = ''
+consumer_key = ''
+consumer_secret = ''
+
+if __name__ == '__main__':
+
+    // get request token
+    
+    // create authenticate url
+    
+    // transition authenticate twitter
+    
