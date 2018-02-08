0cec533a1401a709fc40f7c2880e6a90e94239fe
Coniglio <halfway67@icloud.com>
2018-02-08 15:53:33 +0900

    Update twinail.py

--- twinail.py
+++ twinail.py
@@ -10,11 +10,14 @@ callback_url = ''
 consumer_key = ''
 consumer_secret = ''
 
+def oauth_request(url, key, secret, http_method='GET', post=body='', http_headers=None):
+    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    response_page, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
+    return content
+
 if __name__ == '__main__':
 
-    // get request token
-    
-    // create authenticate url
-    
-    // transition authenticate twitter
-    
+    home_timeline = oauth_req( 'https://api.twitter.com/1.1/statuses/home_timeline.json', 'abcdefg', 'hijklmnop' )
+    print(home_timeline)
