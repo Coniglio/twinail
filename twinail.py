4c0a35994e622c67550729f11b95f5a452587e53
Coniglio <halfway67@icloud.com>
2018-02-08 16:06:01 +0900

    Update twinail.py

--- twinail.py
+++ twinail.py
@@ -9,7 +9,7 @@ def oauth_request(url, key, secret, http_method='GET', post=body='', http_header
     config = configparser.ConfigParser()
     config.read('twitter.ini')
     
-    consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
+    consumer = oauth2.Consumer(key=config['authentication']['consumer_key'], secret=config['authentication']['consumer_secret'])
     token = oauth2.Token(key=key, secret=secret)
     client = oauth2.Client(consumer, token)
     response_page, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
